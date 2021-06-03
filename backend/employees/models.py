9bca691668449ce33bdba7477b7d5d7572369f25
andrey.shishkin <andrey.shishkin@quantori.com>
2021-06-03 21:37:40 +0300

    add app employees to models

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    employee_email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.lastname
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employee'
