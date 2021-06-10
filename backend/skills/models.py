ea68b696059a2d017faafc4b49f3893dd7d2afc9
andrey.shishkin <andrey.shishkin@quantori.com>
2021-06-10 18:56:27 +0300

    model skills

--- backend/skills/models.py
+++ backend/skills/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 
+class Competency(models.TextChoices):
+    Python = 'Python', 'Python'
+    Java = 'java', 'java'
+    JavaScript = 'JavaSctipt', 'JavaSctipt'
+
+
 class Skill(models.Model):
-    COMPETENCY = (
-        (1, 'python'),
-        (2, 'java'),
-        (3, 'js'),
-        (4, '.net'),
-    )
     skill = models.CharField(max_length=200)
-    competency = models.CharField(max_length=200, choices=COMPETENCY)
+    competency = models.CharField(max_length=200, choices=Competency.choices)
 
     def __str__(self):
         return self.skill
