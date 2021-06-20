adf9482cbfa8df5c8f2d0cfe0f4798e7bb56dc67
andrey.shishkin <andrey.shishkin@quantori.com>
2021-06-20 20:46:46 +0300

    add preset and request skill models

--- backend/algorithms/models.py
+++ backend/algorithms/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from skills.models import Skill
+
+
+class Preset(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Preset'
+        verbose_name_plural = 'Presets'
+
+
+class RequestSkill(models.Model):
+    preset_id = models.ForeignKey(Preset, verbose_name='preset', on_delete=models.PROTECT)
+    skill_id = models.ForeignKey(Skill, verbose_name='skill', on_delete=models.PROTECT)
+    is_main = models.BooleanField(null=False)
+    seniority_level = models.IntegerField(null=False)
+
+    class Meta:
+        verbose_name = 'EmployeeSkill'
+        verbose_name_plural = 'EmployeeSkills'
